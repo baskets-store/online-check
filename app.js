d82bb7caba473f56b5d51acb072728cc12d8a745
lauryngrootfaam <grootfaamlauryn@gmail.com>
2022-11-23 15:13:53 +0100

    first version of the online product check for baskets

---
+++ app.js
@@ -0,0 +1,38 @@
+var script = document.createElement('script');
+script.src = "https://unpkg.com/object-exporter@3.2.1/dist/objectexporter.min.js";
+document.getElementsByTagName('head')[0].appendChild(script);
+
+const products = [];
+const exportData = [];
+let pages = [];
+
+async function getHiddenProducts() {
+    await fetch('https://baskets.webshopapp.com/admin/products.json').then(data => data.json()).then(data => pages.push(data.links.pages));
+
+    for (let i = 1; i < pages[0] + 1; i++) {
+        await fetch(`https://baskets.webshopapp.com/admin/products/?page=${i}.json`).then(data => data.json()).then(data => products.push(data.products));
+
+    }
+
+await products.forEach(setProducts =>
+    setProducts.forEach(product => {
+        if (product.visibility == "hidden") {
+           exportData.push({
+                name: product.en.fulltitle,
+                // brand: product.brand.title,
+                url: `https://baskets.webshopapp.com/admin/products/${product.id}`
+            })
+        }
+    })
+)
+
+// await objectExporter({
+//     exportable: exportData,
+//     type: 'csv',
+//     fileName: 'export',
+//     headers: ["Naam", "Merk", "URL"],
+//   })
+
+
+}
+getHiddenProducts();
